e7fe2c3db06459adbac114d8eebe8235e77e4c04
thanhnamgl1609 <thanhnamgl1609@github.com>
2021-11-13 21:05:51 +0700

    Update sign in feature

---
+++ app/src/main/java/com/project/simplecoffee/di /AppModule.kt
@@ -0,0 +1,4 @@
+package com.project.simplecoffee.di 
+
+object AppModule {
+}
--- app/src/main/java/com/project/simplecoffee/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/project/simplecoffee/viewmodels/AuthViewModel.kt
@@ -1,5 +1,7 @@
 package com.project.simplecoffee.viewmodels
 
+import android.app.Application
+import androidx.annotation.NonNull
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseUser
@@ -7,7 +9,8 @@ import com.project.simplecoffee.data.repository.AuthRepo
 
 class ShopViewModel : ViewModel() {
     // TODO: Implement the ViewModel
-    private var repository: AuthRepo? = null
+    private var _repository: AuthRepo? = null
+    private val repository get() = _repository!!
     private val userData: MutableLiveData<FirebaseUser?>? = null
     private val loggedStatus: MutableLiveData<Boolean?>? = null
 
@@ -18,6 +21,7 @@ class ShopViewModel : ViewModel() {
     fun getLoggedStatus(): MutableLiveData<Boolean?>? {
         return loggedStatus
     }
+
     fun signIn(email: String?, pass: String?) {
         repository?.login(email, pass)
     }
